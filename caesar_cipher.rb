f2011c4c5121e2dfc6f368a350d09e034f359b63
John-Rashta <john_rashta@outlook.com>
2025-07-29 19:56:24 +0100

    feat: finish exercise

---
+++ caesar_cipher.rb
@@ -0,0 +1,24 @@
+def caesar_cipher(string, key)
+  #.between?(97, 122) .between?(65, 90)
+  #maybe store both in a variable, is_upper is_lower, than change accordingly
+  ciphered_codes = string.codepoints.map do |number|
+    is_upper = number.between?(65, 90)
+    is_lower = number.between?(97, 122)
+
+    if (is_lower || is_upper)
+      string_case = is_lower ? {max_code: 122, min_code: 97} : {max_code: 90, min_code: 65}
+      ciphered_value = number + key
+      if (ciphered_value > string_case[:max_code])
+        string_case[:min_code] + ((ciphered_value - string_case[:max_code]) - 1)
+      else
+        ciphered_value
+      end
+    else
+      number
+    end
+  end
+
+  ciphered_codes.map { |code| code.chr }.join
+end
+
+p caesar_cipher("What a string!", 5)
